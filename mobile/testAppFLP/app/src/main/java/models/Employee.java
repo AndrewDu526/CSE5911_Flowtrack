c3ecfdeb7966e43d531471eab21745a25a69d92d
AndrewDu526 <changandu526@gmail.com>
2025-09-16 22:14:08 -0400

    chore: init monorepo with mobile app, basic FLP test demo

---
+++ mobile/testAppFLP/app/src/main/java/models/Employee.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.List;
+
+public class Employee {
+
+    String employeeName;
+    String employeeId;
+    public List<EmployeeDevice> devices;
+}
---
+++ mobile/testAppFLP/app/src/main/java/models/TrackPoint.java
@@ -0,0 +1,14 @@
+package models;
+
+// model to store single location point
+public class TrackPoint {
+    public long tWall;       // System.currentTimeMillis()
+    public long tElapsedNs;  // SystemClock.elapsedRealtimeNanos()
+    public double lat, lon; // latitude and longitude
+    public float acc; // accuracy
+    public String provider; // tech provide: GPS/BLT/WIFI...
+    public String source; // tech source: FLP
+    public String priority; // Fine location/Coarse location/Balanced
+    public String floor; // TODO: floor
+}
+
